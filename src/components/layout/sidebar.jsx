7b1e0e53997a249f7a1136388d8ab266a0fa29d8
ParasVarankar23 <parasvarankar235@gmail.com>
2025-05-14 11:07:05 +0530

    changes by Paras in sidebar of admin panel alignment change and also add two label add expense and performance board

--- src/components/layout/sidebar.jsx
+++ src/components/layout/sidebar.jsx
@@ -13,6 +13,8 @@ const menuItems = [
     { label: 'Document', img: '/layout/document.png', href: '/document' },
     { label: 'Leave', img: '/layout/leave.png', href: '/leavetable' },
     { label: 'Salary', img: '/layout/salary.png', href: '/salary' },
+    { label: 'Expense', img: '/layout/salary.png', href: '/salary' },
+    { label: 'Performance Board', img: '/layout/salary.png', href: '/salary' },
     { label: 'Company Policies', img: '/layout/company.png', href: '/companypolicy' },
     { label: 'Logout', img: '/layout/logout.png', href: '/' }
 ]
@@ -22,7 +24,7 @@ export default function Sidebar() {
         <div className="fixed min-h-screen w-1/6 bg-gradient-to-b from-[#018ABE] via-[#65B7D4] to-[#E0E2E3] text-white flex flex-col items-center py-6">
 
             {/* Logo */}
-            <div className="flex flex-col justify-center mb-3">
+            <div className="flex flex-col justify-center items-center mb-3">
                 <Image
                     src="/layout/profile.png"
                     alt="Logo"
@@ -30,8 +32,8 @@ export default function Sidebar() {
                     height={120}
                     className="object-contain"
                 />
-                <p>Prashant Patil</p>
-                <p>prashantpatil@gmail.com</p>
+                <p className='text-center'>Prashant Patil</p>
+                <p className='text-center'>prashantpatil@gmail.com</p>
             </div>
 
             {/* Menu */}
