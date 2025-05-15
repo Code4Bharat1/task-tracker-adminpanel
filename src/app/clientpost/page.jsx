5ec69ddcdd568be3f7028c86d109403004319ecb
Amaan Chauhan <amaanc284@gmail.com>
2025-05-15 17:39:56 +0530

    Add AddDocument and ClientPostHistory components with post management features

---
+++ src/app/clientpost/page.jsx
@@ -0,0 +1,25 @@
+
+import Navbar from '@/components/layout/navbar'
+import Sidebar from '@/components/layout/sidebar'
+import ClientPostHistory from '@/components/postupload/clientpost'
+import React from 'react'
+
+
+
+export default function AddDocument() {
+  return (
+     <div className="min-h-screen md:flex bg-white">
+        {/* Desktop Sidebar Section (visible on md+) */}
+        <div className="md:w-1/6">
+        <Sidebar/>
+        </div>
+      <div className="w-full md:w-5/6 md:flex-1 h-screen bg-white">
+         <Navbar/>
+          <div>
+          
+<ClientPostHistory/>
+    </div>
+    </div>
+    </div>
+  )
+}
